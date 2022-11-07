dd54aea754ff3b0a2704d2140947a93cc3ebc587
Abbie <ac2061@cantab.ac.uk>
2022-11-07 14:03:54 +0000

    dirty bug fixes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use core::cmp::min;
+use std::collections::BTreeMap;
 use std::fs;
 
 const INFINITE_DIST : usize = 100000000;
@@ -16,14 +18,14 @@ struct GraphNode
     node_name: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct Graph
 {
     number_of_nodes: usize,
     edges: Vec<Vec<Edge>>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 struct Node
 {
     index: usize,
@@ -72,8 +74,42 @@ fn construct_graph_from_edges(graph_nodes: &Vec<GraphNode>, edge_data: &str) -> 
         let new_edge_reverse = Edge{index_second:start_index, weight:edge_weight};
 
         // create Edge and add to graph.
-        graph.edges[start_index].push(new_edge);
-        graph.edges[end_index].push(new_edge_reverse)
+        // todo: make this not dumb
+        let mut i = 0;
+        let mut old_edge_weight = INFINITE_DIST;
+        for e in &graph.edges[start_index] {
+            println!("i = {}", i);
+            println!("e.index_second == new_edge.index_second, {} == {}", e.index_second, new_edge.index_second);
+            if e.index_second == new_edge.index_second {
+                old_edge_weight = e.weight;
+                break;
+            }
+            i += 1;
+        }
+
+        if old_edge_weight > new_edge.weight {
+            println!("removal idx = {}", i);
+            if old_edge_weight != INFINITE_DIST {
+                graph.edges[start_index].remove(i);
+            }
+            graph.edges[start_index].push(new_edge);
+        
+            i = 0;
+            for e in &graph.edges[end_index] {
+                println!("i = {}", i);
+                println!("e.index_second == new_edge.index_second, {} == {}", e.index_second, new_edge_reverse.index_second);
+                if e.index_second == new_edge_reverse.index_second {
+                    break;
+                }
+                i += 1;
+            }
+            if old_edge_weight != INFINITE_DIST {
+                graph.edges[end_index].remove(i); 
+            }
+            graph.edges[end_index].push(new_edge_reverse);
+            
+        }
+       
     }
     
     return graph;
@@ -138,8 +174,6 @@ fn get_route(routes_to_find: &str, graph_nodes: Vec<GraphNode>) -> (usize, usize
 
 fn dijkstra(mut start_idx: usize, end_idx: usize, graph: &Graph) -> usize {
     
-
-
     let number_of_nodes = graph.number_of_nodes;
 
     let mut dist = Vec::new();
@@ -147,7 +181,7 @@ fn dijkstra(mut start_idx: usize, end_idx: usize, graph: &Graph) -> usize {
         dist.push(INFINITE_DIST);
     }
 
-    let mut nodes_can_visit : Vec<Node> = Vec::new();
+    let mut nodes_can_visit : BTreeMap<usize, Node> = BTreeMap::new();
     let mut nodes_visited : Vec<usize> = Vec::new();
 
     
@@ -160,18 +194,36 @@ fn dijkstra(mut start_idx: usize, end_idx: usize, graph: &Graph) -> usize {
         // which nodes can we visit
         // todo: rather than dont add if contains, check weight and minimise, update parent
         for i in &graph.edges[start_idx] {
-            if !nodes_can_visit.iter().any(|x| x.index == i.index_second) && (
-                !nodes_visited.contains(&i.index_second) && i.index_second != start_idx){
-                nodes_can_visit.push(Node{index: i.index_second.clone(), parent_idx:start_idx, dist_to_node:i.weight.clone()});
+            // if present, minimise weight
+            // todo: don't use a vec for this, maybe hashmap? 
+            if nodes_can_visit.contains_key(&i.index_second) {
+                //println!("nodes can visit: {:?}", nodes_can_visit);
+                nodes_can_visit.entry(i.index_second).and_modify(|curr_node| curr_node.dist_to_node = min(i.weight + dist[start_idx], curr_node.dist_to_node));
+                //println!("nodes can visit: {:?}", nodes_can_visit);
+            }
+            else if !nodes_visited.contains(&i.index_second) && i.index_second != start_idx {
+                // if not present, and we haven't visited the node
+                println!("nodes can visit: {:?}", nodes_can_visit);
+                nodes_can_visit.insert(i.index_second.clone(), Node{index: i.index_second.clone(), parent_idx:start_idx, dist_to_node:i.weight.clone()});
+                println!("nodes can visit: {:?}", nodes_can_visit);
             }
            
         }
         println!("nodes can visit: {:?}", nodes_can_visit);
         // reverse sort
-        nodes_can_visit.sort_by(|a, b| a.dist_to_node.cmp(&b.dist_to_node));
+        let mut min_weight = INFINITE_DIST;
+        let mut idx = INFINITE_DIST;
+        for (i,node) in &nodes_can_visit {
+            if node.dist_to_node < min_weight{
+                min_weight = node.dist_to_node;
+                idx = node.index;
+            }
+            println!("checking out node {}", i);
+        }
 
-        let closest_node = nodes_can_visit.remove(0);
-
+       
+        
+        let closest_node = nodes_can_visit.remove(&idx).unwrap();
         // go to node and update all the connecting nodes.
         println!("closest node: {}", closest_node.index);
         println!("nodes visited: {:?}", nodes_visited);
@@ -194,6 +246,7 @@ fn main() {
     let (node_data, edge_data, routes_to_find) = read_input("src/uk.txt".to_string());
     let graph_nodes : Vec<GraphNode> = get_nodes(&node_data);
     let graph = construct_graph_from_edges(&graph_nodes, &edge_data);
+    println!("Graph: {:?}", graph);
     let (start_idx, end_idx) = get_route(&routes_to_find, graph_nodes);
     println!("start_idx = {}; end_idx = {}", start_idx, end_idx);
     let dist = dijkstra(start_idx, end_idx, &graph);
@@ -220,11 +273,25 @@ mod tests {
         let contents = "4\nInverness\nGlasgow\nEdinburgh\nNewcastle\n\n5\nInverness Glasgow 167\nInverness Edinburgh 158\nGlasgow Edinburgh 45\nGlasgow Newcastle 145\nEdinburgh Newcastle 107\n\nGlasgow Edinburgh\nEdinburgh Inverness\n\n";
         let data : Vec<&str> = contents.split("\n\n").collect();
     
-        let node_data = data[0].to_string();
-        let edge_data = data[1].to_string();
+        //let node_data = data[0].to_string();
+        //let edge_data = data[1].to_string();
         let routes_to_find = data[2].to_string();
 
         assert_eq!(routes_to_find, "Glasgow Edinburgh\nEdinburgh Inverness");
+    }
+    #[test]
+    fn test_multiple_start_edges_input() {
+        let contents = "4\nInverness\nGlasgow\nEdinburgh\nNewcastle\n\n6\nInverness Glasgow 167\nInverness Edinburgh 158\nGlasgow Edinburgh 45\nGlasgow Newcastle 145\nEdinburgh Newcastle 107\nInverness Glasgow 17\n\nGlasgow Edinburgh\nEdinburgh Inverness\n\n";
+        let data : Vec<&str> = contents.split("\n\n").collect();
+    
+        let node_data = data[0].to_string();
+        let edge_data = data[1].to_string();
+
+        let graph_nodes : Vec<GraphNode> = get_nodes(&node_data);
+        let graph = construct_graph_from_edges(&graph_nodes, &edge_data);
+        // graph should not contain the Inverness->Glasgow 167 path
+        let expected_graph = Graph { number_of_nodes: 4, edges: vec![vec![Edge { index_second: 2, weight: 158 }, Edge { index_second: 1, weight: 17 }], vec![Edge { index_second: 2, weight: 45 }, Edge { index_second: 3, weight: 145 }, Edge { index_second: 0, weight: 17 }], vec![Edge { index_second: 0, weight: 158 }, Edge { index_second: 1, weight: 45 }, Edge { index_second: 3, weight: 107 }], vec![Edge { index_second: 1, weight: 145 }, Edge { index_second: 2, weight: 107 }]] };
+        assert_eq!(expected_graph, graph);
     }
     #[test] 
     fn test_route_extraction() {
@@ -257,21 +324,48 @@ mod tests {
         let dist = dijkstra(start_idx, end_idx, &graph);
         assert_eq!(dist, 5);
     }
-
     #[test]
-    fn test_shorter_route_gets_updated(){
-        // assuming bidirectionality, now the edge weight for middle->end should be updated from 3 to 2.
+    fn test_multiple_start_edges(){
         let start_idx = 0;
         let end_idx = 2;
-        let edges_from_start = vec![Edge{index_second: 1, weight: 2}];
+        let edges_from_start = vec![Edge{index_second: 1, weight: 20}, Edge{index_second: 1, weight: 2}];
         let edges_from_middle = vec![Edge{index_second: 0, weight: 2}, Edge{index_second: 2, weight: 3}];
-        let edges_from_end = vec![Edge{index_second: 1, weight: 2}];
+        let edges_from_end = vec![Edge{index_second: 1, weight: 3}];
 
         let graph = Graph{number_of_nodes:3, edges:vec![edges_from_start, edges_from_middle, edges_from_end]};
 
         let dist = dijkstra(start_idx, end_idx, &graph);
+        assert_eq!(dist, 5);
+    }
+    #[test]
+    fn test_shorter_route_gets_updated(){
+        // assuming bidirectionality, now the edge weight for middle->end should be updated from 3 to 2.
+  
+        let contents = "3\nA\nB\nC\n\n4\nA B 2\nB A 2\nB C 3\nC B 2\n\nA C\n\n";
+        let data : Vec<&str> = contents.split("\n\n").collect();
+    
+        let node_data = data[0].to_string();
+        let edge_data = data[1].to_string();
+
+        let graph_nodes : Vec<GraphNode> = get_nodes(&node_data);
+        let graph = construct_graph_from_edges(&graph_nodes, &edge_data);
+        let expected_graph =  Graph { number_of_nodes: 3, edges: vec![vec![Edge { index_second: 1, weight: 2 }], vec![Edge { index_second: 0, weight: 2 }, Edge { index_second: 2, weight: 2 }], vec![Edge { index_second: 1, weight: 2 }]] };
+        assert_eq!(expected_graph, graph);
+        let dist = dijkstra(0, 2, &graph);
         assert_eq!(dist, 4);
     }
     //todo: add tests for path finding york to birmingham
     //todo: find all routes; do in parallel - look at threading
+    #[test]
+    fn test_edges_not_explicitly_in_both_directions(){
+        let start_idx = 0;
+        let end_idx = 2;
+        let edges_from_start = vec![Edge{index_second: 1, weight: 2}];
+        let edges_from_middle = vec![Edge{index_second: 2, weight: 3}];
+
+        let graph = Graph{number_of_nodes:3, edges:vec![edges_from_start, edges_from_middle]};
+
+        let dist = dijkstra(start_idx, end_idx, &graph);
+        assert_eq!(dist, 5);
+    }
 } 
